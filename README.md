0ecce96c1a565664d2fe7ab884abdaac12194f32
blafoo <blafoo@blafoo.de>
2020-01-08 17:51:51 +0100

    Initiale Version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# bingo
-bingo
+# Bingo? Java Web-Application Entwicklung in der Cloud! 
+
+Der vollständige Source-Code zum Artikel in der c't.
---
+++ src/main/java/de/blafoo/bingo/MainView.java
@@ -0,0 +1,21 @@
+package de.blafoo.bingo;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route
+@PWA(name = "bingo", shortName = "bingo")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        Button button = new Button("Click me",
+                event -> Notification.show("Clicked!"));
+        add(button);
+    }
+}
